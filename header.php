a5fcbe4748d8ad927b9658e3897c242c4ef47e63
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-04-18 17:31:26 -0400

    Actualización de nombre de archivos a PHP y se incluyen imágenes

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>ConIngenio</title>
+        <link rel="stylesheet" href="/src/styles/style.css" />
+    </head>
+    <body>
+        <div class="contenedor">
+            <nav class="navigation-bar">
+                <a href="index.php">ConIngenio</a>
+                <a href="index.php" class="active">Home</a>
+                <a href="servicios.php">Nuestros Servicios</a>
+                <a href="contacto.php">Contáctenos</a>
+                <a href="nosotros.php">Nosotros</a>
+            </nav>
